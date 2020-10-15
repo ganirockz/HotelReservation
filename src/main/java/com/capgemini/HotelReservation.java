8cecb75c188a76b9444cc0100f2581c1397f14f7
ganirockz <ganeshrapeti8@gmail.com>
2020-10-15 07:58:52 +0530

    [Ganesh]Modified: add Hotel in Hotel Reservation system

--- src/main/java/com/capgemini/HotelReservation.java
+++ src/main/java/com/capgemini/HotelReservation.java
@@ -3,15 +3,26 @@ package com.capgemini;
 import java.util.*;
 
 public class HotelReservation {
+	static Scanner sc = new Scanner(System.in);
 	static ArrayList<Hotel> hotelReservationSystem = new ArrayList<Hotel>();
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation");
-		Hotel hotel = new Hotel("Rosewood", 1200);
+		Hotel hotel1 = new Hotel("Lakewood", 110);
+		Hotel hotel2  = new Hotel("Bridgewood",160);
+		Hotel hotel3 = new Hotel("Ridgewood",220);
+		hotelReservationSystem.add(hotel1);
+		hotelReservationSystem.add(hotel2);
+		hotelReservationSystem.add(hotel3);
 		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel(hotel);
+		hotelReservation.addHotel();
 		System.out.println(hotelReservationSystem);
 	}
-	public void addHotel(Hotel hotel) {
+	public void addHotel() {
+		System.out.println("Enter the name of the new hotel");
+		String hotelName = sc.nextLine();
+		System.out.println("Enter the rate of the hotel");
+		int hotelRate = Integer.parseInt(sc.nextLine());
+		Hotel hotel = new Hotel(hotelName,hotelRate);
 		hotelReservationSystem.add(hotel);
 	}
 }
--- src/test/java/com/capgemini/HotelReservationTest.java
+++ src/test/java/com/capgemini/HotelReservationTest.java
@@ -4,9 +4,8 @@ import java.util.*;
 public class HotelReservationTest {
 	@Test
 	public void givenHotelNameAndRateShouldBeAdded() {
-		Hotel hotel = new Hotel("Rosewood",1200);
 		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel(hotel);
+		hotelReservation.addHotel();
 		Assert.assertEquals(1, hotelReservation.hotelReservationSystem.size());
 	}
 }
